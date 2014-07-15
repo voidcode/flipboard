941a1c74ead0b011d05bc428c5f7cb203e097737
voidcode <terkelsorensen@gmail.com>
2014-07-16 01:20:28 +0200

    3d-map begin, now game-like movement

--- js/gview.js
+++ js/gview.js
@@ -5,19 +5,19 @@
 			var ctx = gview.getContext('2d');
 			function draw() {
 				ctx.clearRect(0, 0, gw, gh);
-				bold.draw();
+				if(bold.dir == 90){
+					bold.y = bold.y + 1;
+					bold.draw();
+				}
+				else if(bold.dir == 180){
+					bold.x = bold.x + 1;
+					bold.draw();
+				};
 			}
 			
 			//MAINLOOP********************************************************************
 			function mainloop(){
-				if(bold.dir == 180){
-					bold.x = bold.x + 1;
-					draw();
-				}
-				if(bold.dir == 90){
-					bold.y = bold.y + 1;
-					draw();
-				}
+				draw();
 			}
 			setInterval(mainloop, 1000/FPS);
 			//***************************************************************************
@@ -32,24 +32,28 @@
 				switch(event.key)
 				{
 					case 'w':
+						bold.dir = 180;
 						if(!bold.offset)
 							bold.y -= bold.speed;
 						else
 							bold.y = 0;
 					break;
 					case 's':
+						bold.dir = 180;
 						if(!bold.offset)
 							bold.y += bold.speed;
 						else
 							bold.y = gh;
 					break;
 					case 'a':
+						bold.dir = 90;
 						if(!bold.offset)
 							bold.x -= bold.speed;
 						else
 							bold.x = 0;
 					break;
 					case 'd':
+						bold.dir = 90;
 						if(!bold.offset)
 							bold.x += bold.speed;
 						else
--- js/players.js
+++ js/players.js
@@ -4,8 +4,8 @@ var bold = {
 	y: 0,
 	width: 3,
 	heigth: 3,
-	speed: 1,
-	dir: 180,
+	speed: 4,
+	dir: 90,
 	offset: false,
 	draw: function(){
 		var _gw = gw - this.width;
@@ -24,7 +24,10 @@ var bold = {
 				ctx.fillRect(this.x, this.y, this.width, this.heigth);
 			}
 			if(this.x > _gw){
-				this.x = _gw;	
+				if(this.dir == 180)
+					this.dir = 90;	
+				else
+					this.x = 0;
 				ctx.fillRect(this.x, this.y, this.width, this.heigth);
 			}
 			if(this.y < 0){
@@ -32,7 +35,10 @@ var bold = {
 				ctx.fillRect(this.x, this.y, this.width, this.heigth);
 			}
 			if(this.y > _gh){
-				this.y = _gh;
+				if(!this.dir == 90)
+					this.dir = 180;
+				else
+					this.y = 0;
 				ctx.fillRect(this.x, this.y, this.width, this.heigth);
 			}
 		}
